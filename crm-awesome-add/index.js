dac8e45f25320f98b73521e4434f0f0867149557
Tobias Wright <tobias.wright@gmail.com>
2017-10-06 20:59:55 -0400

    added log

--- crm-awesome-add/index.js
+++ crm-awesome-add/index.js
@@ -1,6 +1,6 @@
 module.exports = function (context, req) {
     req.query.tags = JSON.parse(req.query.tags)
-
+    context.log(req.query)
     context.bindings.outputDocument = req.query
 
     context.done();
